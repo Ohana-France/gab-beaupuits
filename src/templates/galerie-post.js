b045bf7756c112ea35da563571a84f5cd1df8ad2
Bastien FREMONDIERE <bastien.fremondiere@agence-ohana.fr>
2023-07-13 16:06:30 +0200

    Start implementing gallery

--- src/templates/galerie-post.js
+++ src/templates/galerie-post.js
@@ -1,8 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { kebabCase } from "lodash";
 import { Helmet } from "react-helmet";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 
@@ -10,8 +9,6 @@ import Content, { HTMLContent } from "../components/Content";
 export const GaleriePostTemplate = ({
   content,
   contentComponent,
-  description,
-  tags,
   title,
   helmet,
 }) => {
@@ -26,20 +23,7 @@ export const GaleriePostTemplate = ({
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
               {title}
             </h1>
-            <p>{description}</p>
             <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
           </div>
         </div>
       </div>
@@ -53,11 +37,12 @@ GaleriePostTemplate.propTypes = {
   description: PropTypes.string,
   title: PropTypes.string,
   helmet: PropTypes.object,
+  images: PropTypes.array
 };
 
 const GaleriePost = ({ data }) => {
   const { markdownRemark: post } = data;
-
+  console.log(data)
   return (
     <Layout>
       <GaleriePostTemplate
@@ -93,12 +78,25 @@ export const pageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       id
       html
+      fields {
+        slug
+      }
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
-        description
         tags
+        images {
+          childImageSharp {
+            gatsbyImageData(
+              width: 120
+              quality: 100
+              layout: CONSTRAINED
+            )
+
+          }
+        }
       }
     }
   }
+    
 `;
